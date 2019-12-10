9440363269c9f2b14d72584ecfbc376293cccb76
debiandebiandebian <debiandebiandebiandebian@gmail.com>
2019-12-10 14:48:55 +0900

    Added an exception handling.

--- media-daemon/test/make_index.cpp
+++ media-daemon/test/make_index.cpp
@@ -1,6 +1,7 @@
 
 // c++
 #include <iostream>
+#include <filesystem>
 
 // ssulex
 #include <common.hpp>
@@ -10,7 +11,13 @@ int main(int argc, char **argv) {
     if (argc != 2)
         ssulex::report_error("Usage: %s <media path>", argv[0]);
     
-    std::cout << ssulex::make_index(argv[1]) << std::endl;
+    try {
+        std::cout << ssulex::make_index(argv[1]) << std::endl;
+    }
+    catch (std::filesystem::filesystem_error e) {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
 }
 
